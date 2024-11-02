31b06805130317d87b15766dd99e133d71a9813b
sribeirodenise <sribeiro.denise@gmail.com>
2024-11-02 00:07:45 -0300

    init

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+
+    console.log("Clicou")
+
+})
+
+const botaoEstouComSorte = document.querySelector(`form [type="button"]`);
+
+botaoEstouComSorte.addEventListener("click", () => {
+
+    location.href = "https://class.hacode.com.br"
+
+})
+
